601fc1b0953fb0829222fc5b4f0d93e48a49405e
Ben Manes <ben.manes@gmail.com>
2014-12-21 15:58:57 -0800

    Doug's 2nd patch

--- src/test/java/com/github/benmanes/caffeine/cache/ConcurrentHashMap2.java
+++ src/test/java/com/github/benmanes/caffeine/cache/ConcurrentHashMap2.java
@@ -1720,88 +1720,88 @@ public class ConcurrentHashMap2<K,V> extends AbstractMap<K,V>
       int h = spread(key.hashCode());
       V val = null;
       int binCount = 0;
-      for (Node<K,V>[] tab = table;;) {
-          Node<K,V> f; int n, i, fh;
-          if (tab == null || (n = tab.length) == 0) {
-            tab = initTable();
-          } else if ((f = tabAt(tab, i = (n - 1) & h)) == null) {
-              Node<K,V> r = new ReservationNode<K,V>();
-              synchronized (r) {
-                  if (casTabAt(tab, i, null, r)) {
-                      binCount = 1;
-                      Node<K,V> node = null;
-                      try {
-                          if ((val = mappingFunction.apply(key)) != null) {
-                            node = new Node<K,V>(h, key, val, null);
-                          }
-                      } finally {
-                          setTabAt(tab, i, node);
-                      }
-                  }
+      for (Node<K, V>[] tab = table;;) {
+        Node<K, V> f;
+        int n, i, fh;
+        if (tab == null || (n = tab.length) == 0) {
+          tab = initTable();
+        } else if ((f = tabAt(tab, i = (n - 1) & h)) == null) {
+          Node<K, V> r = new ReservationNode<K, V>();
+          synchronized (r) {
+            if (casTabAt(tab, i, null, r)) {
+              binCount = 1;
+              Node<K, V> node = null;
+              try {
+                if ((val = mappingFunction.apply(key)) != null) {
+                  node = new Node<K, V>(h, key, val, null);
+                }
+              } finally {
+                setTabAt(tab, i, node);
               }
-              if (binCount != 0) {
-                break;
-              }
+            }
           }
-          else if ((fh = f.hash) == MOVED) {
-            tab = helpTransfer(tab, f);
-          } else {
-              boolean added = false;
-              synchronized (f) {
-                  if (tabAt(tab, i) == f) {
-                      if (fh >= 0) {
-                          binCount = 1;
-                          for (Node<K,V> e = f;; ++binCount) {
-                              K ek; V ev;
-                              if (e.hash == h &&
-                                  ((ek = e.key) == key ||
-                                   (ek != null && key.equals(ek)))) {
-                                  val = e.val;
-                                  break;
-                              }
-                              Node<K,V> pred = e;
-                              if ((e = e.next) == null) {
-                                  if ((val = mappingFunction.apply(key)) != null) {
-                                      added = true;
-                                      pred.next = new Node<K,V>(h, key, val, null);
-                                  }
-                                  break;
-                              }
-                          }
-                      }
-                      else if (f instanceof TreeBin) {
-                          binCount = 2;
-                          TreeBin<K,V> t = (TreeBin<K,V>)f;
-                          TreeNode<K,V> r, p;
-                          if ((r = t.root) != null &&
-                              (p = r.findTreeNode(h, key, null)) != null) {
-                            val = p.val;
-                          } else if ((val = mappingFunction.apply(key)) != null) {
-                              added = true;
-                              t.putTreeVal(h, key, val);
-                          }
-                      }
-                      else if (f instanceof ReservationNode) {
+          if (binCount != 0) {
+            break;
+          }
+        } else if ((fh = f.hash) == MOVED) {
+          tab = helpTransfer(tab, f);
+        } else {
+          boolean added = false;
+          synchronized (f) {
+            if (tabAt(tab, i) == f) {
+              if (fh >= 0) {
+                binCount = 1;
+                for (Node<K, V> e = f;; ++binCount) {
+                  K ek;
+                  V ev;
+                  if (e.hash == h && ((ek = e.key) == key || (ek != null && key.equals(ek)))) {
+                    val = e.val;
+                    break;
+                  }
+                  Node<K, V> pred = e;
+                  if ((e = e.next) == null) {
+                    if ((val = mappingFunction.apply(key)) != null) {
+                      if (pred.next != null) {
                         throw new IllegalStateException("Recursive update");
                       }
+                      added = true;
+                      pred.next = new Node<K, V>(h, key, val, null);
+                    }
+                    break;
                   }
+                }
+              } else if (f instanceof TreeBin) {
+                binCount = 2;
+                TreeBin<K, V> t = (TreeBin<K, V>) f;
+                TreeNode<K, V> r, p;
+                if ((r = t.root) != null && (p = r.findTreeNode(h, key, null)) != null) {
+                  val = p.val;
+                } else if ((val = mappingFunction.apply(key)) != null) {
+                  added = true;
+                  t.putTreeVal(h, key, val);
+                }
+              } else if (f instanceof ReservationNode) {
+                throw new IllegalStateException("Recursive update");
               }
-              if (binCount != 0) {
-                  if (binCount >= TREEIFY_THRESHOLD) {
-                    treeifyBin(tab, i);
-                  }
-                  if (!added) {
-                    return val;
-                  }
-                  break;
-              }
+            }
           }
+          if (binCount != 0) {
+            if (binCount >= TREEIFY_THRESHOLD) {
+              treeifyBin(tab, i);
+            }
+            if (!added) {
+              return val;
+            }
+            break;
+          }
+        }
       }
       if (val != null) {
         addCount(1L, binCount);
       }
       return val;
-  }
+    }
+
     /**
      * If the value for the specified key is present, attempts to
      * compute a new mapping given the key and its current mapped
