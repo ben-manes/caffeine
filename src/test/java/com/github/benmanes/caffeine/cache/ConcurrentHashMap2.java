4e91ece8d73cc344177b16a09abcabf10d688b7a
Ben Manes <ben.manes@gmail.com>
2014-12-21 15:17:57 -0800

    Update CHM2 to use Doug's instanceof check

--- src/test/java/com/github/benmanes/caffeine/cache/ConcurrentHashMap2.java
+++ src/test/java/com/github/benmanes/caffeine/cache/ConcurrentHashMap2.java
@@ -642,9 +642,6 @@ public class ConcurrentHashMap2<K,V> extends AbstractMap<K,V>
         public final V setValue(V value) {
             throw new UnsupportedOperationException();
         }
-        long getThreadId() {
-            return -1; // invalid thread id; never used
-        }
 
         @Override
         public final boolean equals(Object o) {
@@ -1717,95 +1714,94 @@ public class ConcurrentHashMap2<K,V> extends AbstractMap<K,V>
      */
     @Override
     public V computeIfAbsent(K key, Function<? super K, ? extends V> mappingFunction) {
-        if (key == null || mappingFunction == null) {
-          throw new NullPointerException();
-        }
-        int h = spread(key.hashCode());
-        V val = null;
-        int binCount = 0;
-        for (Node<K,V>[] tab = table;;) {
-            Node<K,V> f; int n, i, fh;
-            if (tab == null || (n = tab.length) == 0) {
-              tab = initTable();
-            } else if ((f = tabAt(tab, i = (n - 1) & h)) == null) {
-                Node<K,V> r = new ReservationNode<K,V>();
-                synchronized (r) {
-                    if (casTabAt(tab, i, null, r)) {
-                        binCount = 1;
-                        Node<K,V> node = null;
-                        try {
-                            if ((val = mappingFunction.apply(key)) != null) {
-                              node = new Node<K,V>(h, key, val, null);
-                            }
-                        } finally {
-                            setTabAt(tab, i, node);
-                        }
-                    }
-                }
-                if (binCount != 0) {
+      if (key == null || mappingFunction == null) {
+        throw new NullPointerException();
+      }
+      int h = spread(key.hashCode());
+      V val = null;
+      int binCount = 0;
+      for (Node<K,V>[] tab = table;;) {
+          Node<K,V> f; int n, i, fh;
+          if (tab == null || (n = tab.length) == 0) {
+            tab = initTable();
+          } else if ((f = tabAt(tab, i = (n - 1) & h)) == null) {
+              Node<K,V> r = new ReservationNode<K,V>();
+              synchronized (r) {
+                  if (casTabAt(tab, i, null, r)) {
+                      binCount = 1;
+                      Node<K,V> node = null;
+                      try {
+                          if ((val = mappingFunction.apply(key)) != null) {
+                            node = new Node<K,V>(h, key, val, null);
+                          }
+                      } finally {
+                          setTabAt(tab, i, node);
+                      }
+                  }
+              }
+              if (binCount != 0) {
+                break;
+              }
+          }
+          else if ((fh = f.hash) == MOVED) {
+            tab = helpTransfer(tab, f);
+          } else {
+              boolean added = false;
+              synchronized (f) {
+                  if (tabAt(tab, i) == f) {
+                      if (fh >= 0) {
+                          binCount = 1;
+                          for (Node<K,V> e = f;; ++binCount) {
+                              K ek; V ev;
+                              if (e.hash == h &&
+                                  ((ek = e.key) == key ||
+                                   (ek != null && key.equals(ek)))) {
+                                  val = e.val;
+                                  break;
+                              }
+                              Node<K,V> pred = e;
+                              if ((e = e.next) == null) {
+                                  if ((val = mappingFunction.apply(key)) != null) {
+                                      added = true;
+                                      pred.next = new Node<K,V>(h, key, val, null);
+                                  }
+                                  break;
+                              }
+                          }
+                      }
+                      else if (f instanceof TreeBin) {
+                          binCount = 2;
+                          TreeBin<K,V> t = (TreeBin<K,V>)f;
+                          TreeNode<K,V> r, p;
+                          if ((r = t.root) != null &&
+                              (p = r.findTreeNode(h, key, null)) != null) {
+                            val = p.val;
+                          } else if ((val = mappingFunction.apply(key)) != null) {
+                              added = true;
+                              t.putTreeVal(h, key, val);
+                          }
+                      }
+                      else if (f instanceof ReservationNode) {
+                        throw new IllegalStateException("Recursive update");
+                      }
+                  }
+              }
+              if (binCount != 0) {
+                  if (binCount >= TREEIFY_THRESHOLD) {
+                    treeifyBin(tab, i);
+                  }
+                  if (!added) {
+                    return val;
+                  }
                   break;
-                }
-            }
-            else if ((fh = f.hash) == MOVED) {
-              tab = helpTransfer(tab, f);
-            } else {
-                if (f.getThreadId() == Thread.currentThread().getId()) {
-                  throw new IllegalStateException("Recursive computeIfAbsent");
-                }
-                boolean added = false;
-                synchronized (f) {
-                    if (tabAt(tab, i) == f) {
-                        if (fh >= 0) {
-                            binCount = 1;
-                            for (Node<K,V> e = f;; ++binCount) {
-                                K ek; V ev;
-                                if (e.hash == h &&
-                                    ((ek = e.key) == key ||
-                                     (ek != null && key.equals(ek)))) {
-                                    val = e.val;
-                                    break;
-                                }
-                                Node<K,V> pred = e;
-                                if ((e = e.next) == null) {
-                                    if ((val = mappingFunction.apply(key)) != null) {
-                                        added = true;
-                                        pred.next = new Node<K,V>(h, key, val, null);
-                                    }
-                                    break;
-                                }
-                            }
-                        }
-                        else if (f instanceof TreeBin) {
-                            binCount = 2;
-                            TreeBin<K,V> t = (TreeBin<K,V>)f;
-                            TreeNode<K,V> r, p;
-                            if ((r = t.root) != null &&
-                                (p = r.findTreeNode(h, key, null)) != null) {
-                              val = p.val;
-                            } else if ((val = mappingFunction.apply(key)) != null) {
-                                added = true;
-                                t.putTreeVal(h, key, val);
-                            }
-                        }
-                    }
-                }
-                if (binCount != 0) {
-                    if (binCount >= TREEIFY_THRESHOLD) {
-                      treeifyBin(tab, i);
-                    }
-                    if (!added) {
-                      return val;
-                    }
-                    break;
-                }
-            }
-        }
-        if (val != null) {
-          addCount(1L, binCount);
-        }
-        return val;
-    }
-
+              }
+          }
+      }
+      if (val != null) {
+        addCount(1L, binCount);
+      }
+      return val;
+  }
     /**
      * If the value for the specified key is present, attempts to
      * compute a new mapping given the key and its current mapped
@@ -2309,21 +2305,14 @@ public class ConcurrentHashMap2<K,V> extends AbstractMap<K,V>
      * A place-holder node used in computeIfAbsent and compute
      */
     static final class ReservationNode<K,V> extends Node<K,V> {
-        final long threadId;
 
         ReservationNode() {
             super(RESERVED, null, null, null);
-            threadId = Thread.currentThread().getId();
         }
 
         @Override
         Node<K,V> find(int h, Object k) {
             return null;
-        }
-
-        @Override
-        long getThreadId() {
-          return threadId;
         }
     }
 
