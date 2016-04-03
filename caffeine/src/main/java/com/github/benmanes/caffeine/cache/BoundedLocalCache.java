f16a0c5f2bbe3afb840ba8310692e8424b804b7d
Ben Manes <ben.manes@gmail.com>
2016-04-02 19:50:48 -0700

    Fix minor PMD warnings (unnecessary finals)

--- caffeine/src/main/java/com/github/benmanes/caffeine/cache/BoundedLocalCache.java
+++ caffeine/src/main/java/com/github/benmanes/caffeine/cache/BoundedLocalCache.java
@@ -2654,20 +2654,20 @@ abstract class BoundedLocalCache<K, V> extends BLCHeader.DrainStatusRef<K, V>
     }
   }
 
-  /** A reusable task that performs the maintenance work used to avoid wrapping by ForkJoinPool. */
+  /** A reusable task that performs the maintenance work; used to avoid wrapping by ForkJoinPool. */
   final class PerformCleanupTask extends ForkJoinTask<Void> implements Runnable {
     private static final long serialVersionUID = 1L;
 
     @Override
-    public final Void getRawResult() {
+    public Void getRawResult() {
       return null;
     }
 
     @Override
-    public final void setRawResult(Void v) {}
+    public void setRawResult(Void v) {}
 
     @Override
-    public final boolean exec() {
+    public boolean exec() {
       performCleanUp();
       return true;
     }
