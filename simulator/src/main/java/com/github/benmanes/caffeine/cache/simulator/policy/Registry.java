d4944cf115f67ad4244e5e204bade603d23138b6
Ohad Eytan <OhadEy@gmail.com>
2018-09-13 00:52:28 +0300

    Add indicator version of FRD policy

--- simulator/src/main/java/com/github/benmanes/caffeine/cache/simulator/policy/Registry.java
+++ simulator/src/main/java/com/github/benmanes/caffeine/cache/simulator/policy/Registry.java
@@ -33,6 +33,7 @@ import com.github.benmanes.caffeine.cache.simulator.policy.adaptive.CarPolicy;
 import com.github.benmanes.caffeine.cache.simulator.policy.adaptive.CartPolicy;
 import com.github.benmanes.caffeine.cache.simulator.policy.irr.ClockProPolicy;
 import com.github.benmanes.caffeine.cache.simulator.policy.irr.FrdPolicy;
+import com.github.benmanes.caffeine.cache.simulator.policy.irr.IndicatorFrdPolicy;
 import com.github.benmanes.caffeine.cache.simulator.policy.irr.LirsPolicy;
 import com.github.benmanes.caffeine.cache.simulator.policy.linked.FrequentlyUsedPolicy;
 import com.github.benmanes.caffeine.cache.simulator.policy.linked.LinkedPolicy;
@@ -155,6 +156,7 @@ public final class Registry {
 
   private static void registerIrr(Map<String, Function<Config, Set<Policy>>> factories) {
     factories.put("irr.Frd", FrdPolicy::policies);
+    factories.put("irr.IndicatorFrd", IndicatorFrdPolicy::policies); 
     factories.put("irr.Lirs", LirsPolicy::policies);
     factories.put("irr.ClockPro", ClockProPolicy::policies);
   }
