8ddbc6a3c0c9047553492bd6a28ed7e119810c1f
xiecongle <289243011@qq.com>
2019-03-12 03:36:52 +0800

    fix a wrong argument check while configure recordStats by CaffeineSpec

--- caffeine/src/main/java/com/github/benmanes/caffeine/cache/CaffeineSpec.java
+++ caffeine/src/main/java/com/github/benmanes/caffeine/cache/CaffeineSpec.java
@@ -271,7 +271,7 @@ public final class CaffeineSpec {
 
   /** Configures the value as weak or soft references. */
   void recordStats(@Nullable String value) {
-    requireArgument(value == null, "record stats does not take a value");
+    requireArgument(value != null, "record stats does not take a value");
     requireArgument(!recordStats, "record stats was already set");
     recordStats = true;
   }
