30ad8e9e22d2bca5b55daf26ed6ddd05ec736737
Guus C. Bloemsma <guus@bloemsma.net>
2019-05-11 17:03:03 +0200

    better interface for different usecases

--- examples/coalescing-bulkloader/src/main/java/com/github/benmanes/caffeine/examples/coalescing/bulkloader/CoalescingBulkloader.java
+++ examples/coalescing-bulkloader/src/main/java/com/github/benmanes/caffeine/examples/coalescing/bulkloader/CoalescingBulkloader.java
@@ -16,10 +16,15 @@
 package com.github.benmanes.caffeine.examples.coalescing.bulkloader;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.stream.Collectors.toMap;
 
 import com.github.benmanes.caffeine.cache.AsyncCacheLoader;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Queue;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -28,6 +33,9 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Stream;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 /**
@@ -39,7 +47,8 @@ import org.checkerframework.checker.nullness.qual.NonNull;
  * @param <Value> the type of the value in the cache
  * @author complain to: guus@bloemsma.net
  */
-public abstract class CoalescingBulkloader<Key, Value> implements AsyncCacheLoader<Key, Value> {
+public class CoalescingBulkloader<Key, Value> implements AsyncCacheLoader<Key, Value> {
+    private final Consumer<Collection<WaitingKey>> bulkLoader;
     private int maxLoadSize; // maximum number of keys to load in one call
     private long maxDelay; // maximum time between request of a value and loading it
     private volatile Queue<WaitingKey> waitingKeys = new ConcurrentLinkedQueue<>();
@@ -50,11 +59,82 @@ public abstract class CoalescingBulkloader<Key, Value> implements AsyncCacheLoad
 
     private final class WaitingKey {
         Key key;
-        CompletableFuture<Value> value;
+        CompletableFuture<Value> future;
         long waitingSince;
     }
 
-    public CoalescingBulkloader(int maxLoadSize, long maxDelay) {
+    /**
+     * Wraps a bulk loader that returns values in the same order as the keys.
+     *
+     * @param maxLoadSize Maximum number of keys per bulk load
+     * @param maxDelay    Maximum time to wait before bulk load is executed
+     * @param load        Loader that takes keys and returns a future with the values in the same order as the keys.
+     *                    Extra values are ignored. Missing values lead to a {@link java.util.NoSuchElementException}
+     *                    for the corresponding future.
+     */
+    public static <Key, Value> CoalescingBulkloader<Key, Value> byOrder(int maxLoadSize, long maxDelay,
+            final Function<Stream<Key>, CompletableFuture<Stream<Value>>> load) {
+        return new CoalescingBulkloader<>(maxLoadSize, maxDelay, toLoad -> {
+            final Stream<Key> keys = toLoad.stream().map(wk -> wk.key);
+            load.apply(keys).thenAccept(values -> {
+                final Iterator<Value> iv = values.iterator();
+                for (CoalescingBulkloader<Key, Value>.WaitingKey waitingKey : toLoad) {
+                    if (iv.hasNext())
+                        waitingKey.future.complete(iv.next());
+                    else
+                        waitingKey.future.completeExceptionally(new NoSuchElementException("No value for key " + waitingKey.key));
+                }
+            });
+        });
+    }
+
+    /**
+     * Wraps a bulk loader that returns values in a map accessed by key.
+     *
+     * @param maxLoadSize Maximum number of keys per bulk load
+     * @param maxDelay    Maximum time to wait before bulk load is executed
+     * @param load        Loader that takes keys and returns a future with a map with keys and values.
+     *                    Extra values are ignored. Missing values lead to a {@link java.util.NoSuchElementException}
+     *                    for the corresponding future.
+     */
+    public static <Key, Value> CoalescingBulkloader<Key, Value> byMap(int maxLoadSize, long maxDelay,
+            final Function<Stream<Key>, CompletableFuture<Map<Key, Value>>> load) {
+        return new CoalescingBulkloader<>(maxLoadSize, maxDelay, toLoad -> {
+            final Stream<Key> keys = toLoad.stream().map(wk -> wk.key);
+            load.apply(keys).thenAccept(values -> {
+                for (CoalescingBulkloader<Key, Value>.WaitingKey waitingKey : toLoad) {
+                    if (values.containsKey(waitingKey.key))
+                        waitingKey.future.complete(values.get(waitingKey.key));
+                    else
+                        waitingKey.future.completeExceptionally(new NoSuchElementException("No value for key " + waitingKey.key));
+                }
+            });
+        });
+    }
+
+    /**
+     * Wraps a bulk loader that returns intermediate values from which keys and values can be extracted.
+     *
+     * @param <Intermediate> Some internal type from which keys and values can be extracted.
+     * @param maxLoadSize    Maximum number of keys per bulk load
+     * @param maxDelay       Maximum time to wait before bulk load is executed
+     * @param keyExtractor   How to extract key from intermediate value
+     * @param valueExtractor How to extract value from intermediate value
+     * @param load           Loader that takes keys and returns a future with a map with keys and values.
+     *                       Extra values are ignored. Missing values lead to a {@link java.util.NoSuchElementException}
+     *                       for the corresponding future.
+     */
+    public static <Key, Value, Intermediate> CoalescingBulkloader<Key, Value> byExtraction(int maxLoadSize, long maxDelay,
+            final Function<Intermediate, Key> keyExtractor,
+            final Function<Intermediate, Value> valueExtractor,
+            final Function<Stream<Key>, CompletableFuture<Stream<Intermediate>>> load) {
+        return byMap(maxLoadSize, maxDelay,
+                keys -> load.apply(keys).thenApply(
+                        intermediates -> intermediates.collect(toMap(keyExtractor, valueExtractor))));
+    }
+
+    private CoalescingBulkloader(int maxLoadSize, long maxDelay, Consumer<Collection<WaitingKey>> bulkLoader) {
+        this.bulkLoader = bulkLoader;
         assert maxLoadSize > 0;
         assert maxDelay > 0;
         this.maxLoadSize = maxLoadSize;
@@ -62,20 +142,19 @@ public abstract class CoalescingBulkloader<Key, Value> implements AsyncCacheLoad
     }
 
     @Override public @NonNull CompletableFuture<Value> asyncLoad(@NonNull Key key, @NonNull Executor executor) {
-        if (schedule == null || schedule.isDone()) {
-            startWaiting();
-        }
-
         final WaitingKey waitingKey = new WaitingKey();
         waitingKey.key = key;
-        waitingKey.value = new CompletableFuture<>();
+        waitingKey.future = new CompletableFuture<>();
         waitingKey.waitingSince = System.currentTimeMillis();
         waitingKeys.add(waitingKey);
 
         if (size.incrementAndGet() >= maxLoadSize) {
             doLoad();
+        } else if (schedule == null || schedule.isDone()) {
+            startWaiting();
         }
-        return waitingKey.value;
+
+        return waitingKey.future;
     }
 
     synchronized private void startWaiting() {
@@ -85,21 +164,21 @@ public abstract class CoalescingBulkloader<Key, Value> implements AsyncCacheLoad
     synchronized private void doLoad() {
         schedule.cancel(false);
         do {
-            Map<Key, CompletableFuture<Value>> toLoad = new HashMap<>(Math.min(size.get(), maxLoadSize));
+            List<WaitingKey> toLoad = new ArrayList<>(Math.min(size.get(), maxLoadSize));
             int counter = maxLoadSize;
             while (counter > 0) {
                 final WaitingKey waitingKey = waitingKeys.poll();
                 if (waitingKey == null)
                     break;
                 else {
-                    toLoad.put(waitingKey.key, waitingKey.value);
+                    toLoad.add(waitingKey);
                     counter--;
                 }
             }
 
             final int taken = maxLoadSize - counter;
             if (taken > 0) {
-                load(toLoad);
+                bulkLoader.accept(toLoad);
                 size.updateAndGet(oldSize -> oldSize - taken);
             }
 
@@ -109,11 +188,5 @@ public abstract class CoalescingBulkloader<Key, Value> implements AsyncCacheLoad
             schedule = timer.schedule(this::doLoad, nextWaitingKey.waitingSince + maxDelay - System.currentTimeMillis(), MILLISECONDS);
         }
     }
-
-    /**
-     * Must be implemented by user. It is expected to <b>asynchronously</b> load all keys and complete the corresponding futures
-     * with the correct values. When done each future <b>must</b> be completed with the correct value.
-     */
-    protected abstract void load(Map<Key, CompletableFuture<Value>> toLoad);
 
 }
