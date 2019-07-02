10dc59c29705bf43c2bd690e9a558a1e90b67cbb
Guus C. Bloemsma <guus@bloemsma.net>
2019-07-02 14:55:40 +0200

    loadtime?

--- examples/coalescing-bulkloader/src/main/java/com/github/benmanes/caffeine/examples/coalescing/bulkloader/CoalescingBulkloader.java
+++ examples/coalescing-bulkloader/src/main/java/com/github/benmanes/caffeine/examples/coalescing/bulkloader/CoalescingBulkloader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Wim Deblauwe. All Rights Reserved.
+ * Copyright 2019 Guus C. Bloemsma. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
