7d7f7751b4a1a25584db6515424cc87fe2d152d2
Christophe Roudet <croudet@stratacache.com>
2019-11-05 15:33:26 -0500

    fix: disable archive publishing on PR.

--- travis.sh
+++ travis.sh
@@ -30,7 +30,7 @@ case "${1:?''}" in
     run "./gradlew check --console plain"
     runSlow "./gradlew :caffeine:slowCaffeineTest --console plain"
     runSlow "./gradlew :caffeine:slowGuavaTest --console plain"
-    if [[ (${CI:-false} == "true") || (${TRAVIS_PULL_REQUEST:-false} != "false") ]]; then
+    if [[ (${CI:-false} == "true") && (${TRAVIS_PULL_REQUEST:-false} != "false") ]]; then
       run "./gradlew coveralls uploadArchives --console plain"
       runSlow "./gradlew sonarqube --console plain"
     fi
