9ed02ec4e7c65270a222c733dd937a7f4c2656a1
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2019-11-20 13:33:37 -0300

    Make BoundedBuffer a public class so that it can be used in different contexts

--- caffeine/src/main/java/com/github/benmanes/caffeine/cache/BoundedBuffer.java
+++ caffeine/src/main/java/com/github/benmanes/caffeine/cache/BoundedBuffer.java
@@ -28,7 +28,7 @@ import com.github.benmanes.caffeine.base.UnsafeAccess;
  * @author ben.manes@gmail.com (Ben Manes)
  * @param <E> the type of elements maintained by this buffer
  */
-final class BoundedBuffer<E> extends StripedBuffer<E> {
+public final class BoundedBuffer<E> extends StripedBuffer<E> {
   /*
    * A circular ring buffer stores the elements being transferred by the producers to the consumer.
    * The monotonically increasing count of reads and writes allow indexing sequentially to the next
