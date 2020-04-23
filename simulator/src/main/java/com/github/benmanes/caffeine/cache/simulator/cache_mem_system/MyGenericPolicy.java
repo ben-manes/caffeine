61407c99098b8e9f6e09a700ba8bb835ca2d96a9
ofanan <ofanan@gmail.com>
2020-04-23 15:31:20 +0300

    Added MyGenericPolicy, which extends MyLinkedPolicy
    file:///C:/Users/ofanan/Documents/GitHub/caffeine/simulator/src/main/java/com/github/benmanes/caffeine/cache/simulator/cache_mem_system/MyGenericPolicy.java
    file:///C:/Users/ofanan/Documents/GitHub/caffeine/simulator/src/main/java/com/github/benmanes/caffeine/cache/simulator/policy/linked/MyLinkedPolicy.java
    file:///C:/Users/ofanan/Documents/GitHub/caffeine/simulator/src/main/java/scala-library-2.3.1.jar

---
+++ simulator/src/main/java/com/github/benmanes/caffeine/cache/simulator/cache_mem_system/MyGenericPolicy.java
@@ -0,0 +1,35 @@
+package com.github.benmanes.caffeine.cache.simulator.cache_mem_system;
+
+import static com.github.benmanes.caffeine.cache.simulator.policy.Policy.Characteristic.WEIGHTED;
+import com.github.benmanes.caffeine.cache.simulator.cache_mem_system.*;
+import static java.util.Locale.US;
+import static java.util.stream.Collectors.toSet;
+
+import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.github.benmanes.caffeine.cache.simulator.BasicSettings;
+import com.github.benmanes.caffeine.cache.simulator.admission.Admission;
+import com.github.benmanes.caffeine.cache.simulator.admission.Admittor;
+import com.github.benmanes.caffeine.cache.simulator.policy.AccessEvent;
+import com.github.benmanes.caffeine.cache.simulator.policy.Policy;
+import com.github.benmanes.caffeine.cache.simulator.policy.PolicyStats;
+import com.github.benmanes.caffeine.cache.simulator.policy.Policy.Characteristic;
+import com.github.benmanes.caffeine.cache.simulator.policy.linked.MyLinkedPolicy;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.collect.Sets;
+import com.typesafe.config.Config;
+
+import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
+import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
+
+public class MyGenericPolicy extends MyLinkedPolicy {
+  public CacheMemSystem cache_mem_system;
+  
+  public MyGenericPolicy (Admission admission, EvictionPolicy policy, Config config) {
+    super (admission, policy, config);
+    cache_mem_system = new CacheMemSystem ();
+  }
+}
