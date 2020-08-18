ac014b531aace241a423968697179737ceb4f926
Radu Cotescu <cotescu@adobe.com>
2020-08-18 13:30:47 +0200

    fix #447: Allow the CaffeineCachingProvider to be used with OSGi DS
    * annotated CaffeineCachingProvider to make it an OSGi service
    * when running in an OSGi container, set the CaffeineCachingProvider's
    classloader as the Thread context classloader for every API call on the
    provided CacheManagerImpl - this makes sure that the default config is
    read from the bundle; the default classloader will do a fallback to
    the app classloader, allowing to override / define configurations
    * added an OSGi DS test
    * made the bnd build reproducible (no extra bundle headers)

--- jcache/src/test/java/com/github/benmanes/caffeine/jcache/OSGiTest.java
+++ jcache/src/test/java/com/github/benmanes/caffeine/jcache/OSGiTest.java
@@ -15,6 +15,8 @@
  */
 package com.github.benmanes.caffeine.jcache;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.ops4j.pax.exam.CoreOptions.bundle;
 import static org.ops4j.pax.exam.CoreOptions.junitBundles;
@@ -24,6 +26,7 @@ import static org.ops4j.pax.exam.CoreOptions.options;
 import javax.cache.Cache;
 import javax.cache.Caching;
 import javax.cache.spi.CachingProvider;
+import javax.inject.Inject;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -33,12 +36,17 @@ import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerMethod;
 
+import com.github.benmanes.caffeine.jcache.spi.CaffeineCachingProvider;
+
 /**
  * @author ben.manes@gmail.com (Ben Manes)
  */
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerMethod.class)
 public final class OSGiTest {
+
+  @Inject
+  private CachingProvider cachingProvider;
 
   @Configuration
   public Option[] config() {
@@ -47,7 +55,10 @@ public final class OSGiTest {
         bundle("file:" + System.getProperty("caffeine.osgi.jar")),
         bundle("file:" + System.getProperty("caffeine-jcache.osgi.jar")),
         mavenBundle("com.typesafe", "config", System.getProperty("config.osgi.version")),
-        mavenBundle("javax.cache", "cache-api", System.getProperty("jcache.osgi.version")));
+        mavenBundle("javax.cache", "cache-api", System.getProperty("jcache.osgi.version")),
+        mavenBundle("org.apache.felix", "org.apache.felix.scr", System.getProperty("felixScr.version")),
+        mavenBundle().groupId("org.osgi").artifactId("org.osgi.util.function").version(System.getProperty("osgiUtil.function")),
+        mavenBundle().groupId("org.osgi").artifactId("org.osgi.util.promise").version(System.getProperty("osgiUtil.promise")));
   }
 
   @Test
@@ -58,5 +69,11 @@ public final class OSGiTest {
     Cache<String, Integer> cache = cachingProvider.getCacheManager()
         .getCache("test-cache-2", String.class, Integer.class);
     assertNull(cache.get("a"));
+  }
+
+  @Test
+  public void testOSGIDS() {
+    assertNotNull("Should have found a registered CachingProvider.", cachingProvider);
+    assertEquals(CaffeineCachingProvider.class, cachingProvider.getClass());
   }
 }
