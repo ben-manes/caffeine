bd19a3cf3c5b10fe1214026b3ef6e3272f1e1800
Aleksei Semin <alllexsm@gmail.com>
2020-11-02 16:40:21 +0100

    Verify maximumWeight before setting

--- caffeine/src/main/java/com/github/benmanes/caffeine/cache/Caffeine.java
+++ caffeine/src/main/java/com/github/benmanes/caffeine/cache/Caffeine.java
@@ -407,8 +407,8 @@ public final class Caffeine<K, V> {
         "maximum weight was already set to %s", this.maximumWeight);
     requireState(this.maximumSize == UNSET_INT,
         "maximum size was already set to %s", this.maximumSize);
-    this.maximumWeight = maximumWeight;
     requireArgument(maximumWeight >= 0, "maximum weight must not be negative");
+    this.maximumWeight = maximumWeight;
     return this;
   }
 
