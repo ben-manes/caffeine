a8c274ddb0e50c591a9927dd3defc7bd82a4828d
Ben Manes <ben.manes@gmail.com>
2021-03-07 00:35:59 -0800

    Add putIfAbsent optimistic fastpath (fixes #506)
    Put optimistically reads the existing value to avoid a map lambda and
    hash table lock. If absent, it then tries to add a new entry. If this
    fails when racing with another writer, a putIfAbsent can validate and
    return the existing value immediately. Previously it would do these
    steps after acquiring the entry lock during the update code path.
    
    This mirrors the fastpath when the entry was found and no update is
    needed for a putIfAbsent. That case will be more common, this only
    assists when populating, e.g. after expiration when many threads
    may try to reload the value.

--- caffeine/src/main/java/com/github/benmanes/caffeine/cache/BoundedLocalCache.java
+++ caffeine/src/main/java/com/github/benmanes/caffeine/cache/BoundedLocalCache.java
@@ -2086,6 +2086,17 @@ abstract class BoundedLocalCache<K, V> extends BLCHeader.DrainStatusRef<K, V>
         if (prior == null) {
           afterWrite(new AddTask(node, newWeight));
           return null;
+        } else if (onlyIfAbsent) {
+          // An optimistic fast path to avoid unnecessary locking
+          V currentValue = prior.getValue();
+          if ((currentValue != null) && !hasExpired(prior, now)) {
+            if (!isComputingAsync(prior)) {
+              tryExpireAfterRead(prior, key, currentValue, expiry(), now);
+              setAccessTime(prior, now);
+            }
+            afterRead(prior, now, /* recordHit */ false);
+            return currentValue;
+          }
         }
       } else if (onlyIfAbsent) {
         // An optimistic fast path to avoid unnecessary locking
