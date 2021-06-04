a8af2a29f81e919c787e9ee0d2577339707fa3c3
soc <simon@ochsenreither.de>
2021-06-04 21:56:11 +0200

    Drop `transitive` from annotations Caffeine requires

--- caffeine/src/main/java/module-info.java
+++ caffeine/src/main/java/module-info.java
@@ -2,6 +2,6 @@ module com.github.benmanes.caffeine {
   exports com.github.benmanes.caffeine.cache;
   exports com.github.benmanes.caffeine.cache.stats;
 
-  requires static transitive com.google.errorprone.annotations;
-  requires static transitive org.checkerframework.checker.qual;
+  requires static com.google.errorprone.annotations;
+  requires static org.checkerframework.checker.qual;
 }
