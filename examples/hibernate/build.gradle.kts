f025f804df1399fdc706396f6f181b1d9fbff1c7
Ben Manes <ben.manes@gmail.com>
2023-09-09 18:20:29 -0700

    update hibernate example to use the preferred meta model generator

--- examples/hibernate/build.gradle.kts
+++ examples/hibernate/build.gradle.kts
@@ -1,9 +1,15 @@
+import org.gradle.plugins.ide.eclipse.model.Classpath
+import org.gradle.plugins.ide.eclipse.model.SourceFolder
+
 plugins {
+  eclipse
   `java-library`
   alias(libs.plugins.versions)
 }
 
 dependencies {
+  annotationProcessor(libs.hibernate.jpamodelgen)
+
   implementation(libs.bundles.hibernate)
   implementation(libs.bundles.log4j2)
   implementation(libs.caffeine)
@@ -25,5 +31,12 @@ java.toolchain.languageVersion = JavaLanguageVersion.of(
 tasks.withType<JavaCompile>().configureEach {
   javaCompiler = javaToolchains.compilerFor {
     languageVersion = java.toolchain.languageVersion
+  }
+}
+
+eclipse.classpath.file.beforeMerged {
+  if (this is Classpath) {
+    val absolutePath = layout.buildDirectory.dir("generated/sources/annotationProcessor/java/main")
+    entries.add(SourceFolder(relativePath(absolutePath), "bin/main"))
   }
 }
--- examples/hibernate/src/test/java/com/github/benmanes/caffeine/examples/hibernate/HibernateCacheTest.java
+++ examples/hibernate/src/test/java/com/github/benmanes/caffeine/examples/hibernate/HibernateCacheTest.java
@@ -17,109 +17,109 @@ package com.github.benmanes.caffeine.examples.hibernate;
 
 import static com.github.benmanes.caffeine.examples.hibernate.HibernateSubject.assertThat;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
  * @author ben.manes@gmail.com (Ben Manes)
  */
 public final class HibernateCacheTest {
-  SessionFactory sessionFactory;
-  Repository repository;
-
-  @BeforeEach
-  public void beforeEach() {
-    sessionFactory = new Configuration().addAnnotatedClass(User.class)
-        .addAnnotatedClass(Skill.class).addAnnotatedClass(Project.class)
-        .buildSessionFactory(new StandardServiceRegistryBuilder().build());
-    repository = new Repository(sessionFactory);
-  }
-
-  @AfterEach
-  public void afterEach() {
-    sessionFactory.close();
-  }
 
   @Test
   public void lookup() {
-    long projectId = createData("Ben", "Caffeine", "Hibernate");
+    try (var sessionFactory = newSessionFactory()) {
+      long projectId = sessionFactory.fromTransaction(session ->
+          createData(session, "Ben", "Caffeine", "Hibernate"));
 
-    // db hit
-    Project project = repository.getProject(projectId);
+      // db hit
+      var project = sessionFactory.fromSession(session -> Queries_.getProject(session, projectId));
 
-    // cache hit
-    project = repository.getProject(projectId);
-    assertThat(sessionFactory).hits(4).misses(0).puts(3);
+      // cache hit
+      sessionFactory.inSession(session -> Queries_.getProject(session, projectId));
+      assertThat(sessionFactory).hits(4).misses(0).puts(3);
 
-    // cache hit due to project associations
-    repository.getUser(project.getAssignee().getId());
-    assertThat(sessionFactory).hits(5).misses(0).puts(3);
+      // cache hit due to project associations
+      sessionFactory.inSession(session -> Queries_.getUser(session, project.getAssignee().getId()));
+      assertThat(sessionFactory).hits(5).misses(0).puts(3);
 
-    // cache hit due to only the project being explicitly evicted
-    repository.evictProject(projectId);
-    repository.getUser(project.getAssignee().getId());
-    assertThat(sessionFactory).hits(6).misses(0).puts(3);
+      // cache hit due to only the project being explicitly evicted
+      sessionFactory.getCache().evict(Project.class, projectId);
+      sessionFactory.inSession(session -> Queries_.getUser(session, project.getAssignee().getId()));
+      assertThat(sessionFactory).hits(6).misses(0).puts(3);
 
-    // db hit
-    project = repository.getProject(projectId);
-    assertThat(sessionFactory).hits(6).misses(1).puts(4);
+      // db hit
+      sessionFactory.inSession(session -> Queries_.getProject(session, projectId));
+      assertThat(sessionFactory).hits(6).misses(1).puts(4);
+    }
   }
 
   @Test
   public void query() {
-    long projectId = createData("Ben", "Caffeine", "Hibernate");
+    try (var sessionFactory = newSessionFactory()) {
+      long projectId = sessionFactory.fromTransaction(session ->
+          createData(session, "Ben", "Caffeine", "Hibernate"));
 
-    // db hit
-    repository.findProject(projectId);
-    assertThat(sessionFactory).hits(1).misses(0).puts(3);
+      // db hit
+      sessionFactory.inSession(session -> Queries_.findProject(session, projectId));
+      assertThat(sessionFactory).hits(1).misses(0).puts(3);
 
-    // cache hit
-    repository.findProject(projectId);
-    assertThat(sessionFactory).hits(2).misses(0).puts(3);
+      // cache hit
+      sessionFactory.inSession(session -> Queries_.findProject(session, projectId));
+      assertThat(sessionFactory).hits(2).misses(0).puts(3);
 
-    // db hit
-    repository.findProjects();
-    assertThat(sessionFactory).hits(3).misses(0).puts(3);
+      // db hit
+      sessionFactory.inSession(session -> Queries_.findProjects(session));
+      assertThat(sessionFactory).hits(3).misses(0).puts(3);
 
-    // cache hit
-    repository.findProjects();
-    assertThat(sessionFactory).hits(4).misses(0).puts(3);
+      // cache hit
+      sessionFactory.inSession(session -> Queries_.findProjects(session));
+      assertThat(sessionFactory).hits(4).misses(0).puts(3);
 
-    // db hit
-    repository.evictAll();
-    repository.findProject(projectId);
-    assertThat(sessionFactory).hits(5).misses(0).puts(3);
+      // db hit
+      sessionFactory.getCache().evictDefaultQueryRegion();
+      sessionFactory.inSession(session -> Queries_.findProject(session, projectId));
+      assertThat(sessionFactory).hits(5).misses(0).puts(3);
 
-    // query cache hit
-    repository.findProject(projectId);
-    assertThat(sessionFactory).hits(6).misses(0).puts(3);
+      // query cache hit
+      sessionFactory.inSession(session -> Queries_.findProject(session, projectId));
+      assertThat(sessionFactory).hits(6).misses(0).puts(3);
 
-    // db hit
-    repository.findProjects();
-    assertThat(sessionFactory).hits(7).misses(0).puts(3);
+      // db hit
+      sessionFactory.inSession(session -> Queries_.findProjects(session));
+      assertThat(sessionFactory).hits(7).misses(0).puts(3);
 
-    // cache hit
-    repository.findProjects();
-    assertThat(sessionFactory).hits(8).misses(0).puts(3);
+      // cache hit
+      sessionFactory.inSession(session -> Queries_.findProjects(session));
+      assertThat(sessionFactory).hits(8).misses(0).puts(3);
 
-    // automatically evicts project-related queries in the cache
-    repository.updateProject(projectId, "Updated");
-    assertThat(sessionFactory).hits(10).misses(0).puts(4);
+      // automatically evicts project-related queries in the cache
+      sessionFactory.inTransaction(session -> {
+        var project = Queries_.getProject(session, projectId);
+        project.setName("Updated");
+        session.merge(project);
+      });
+      assertThat(sessionFactory).hits(10).misses(0).puts(4);
 
-    // db hit
-    repository.findProject(projectId);
-    assertThat(sessionFactory).hits(11).misses(0).puts(4);
+      // db hit
+      sessionFactory.inSession(session -> Queries_.findProject(session, projectId));
+      assertThat(sessionFactory).hits(11).misses(0).puts(4);
 
-    // db hit
-    repository.findProjects();
-    assertThat(sessionFactory).hits(12).misses(0).puts(4);
+      // db hit
+      sessionFactory.inSession(session -> Queries_.findProjects(session));
+      assertThat(sessionFactory).hits(12).misses(0).puts(4);
+    }
   }
 
-  private long createData(String userName, String projectName, String skillName) {
+  private static SessionFactory newSessionFactory() {
+    return new Configuration().addAnnotatedClass(User.class)
+        .addAnnotatedClass(Skill.class).addAnnotatedClass(Project.class)
+        .buildSessionFactory(new StandardServiceRegistryBuilder().build());
+  }
+
+  private long createData(Session session, String userName, String projectName, String skillName) {
     var project = new Project();
     var skill = new Skill();
     var user = new User();
@@ -133,7 +133,9 @@ public final class HibernateCacheTest {
 
     skill.setName(skillName);
 
-    repository.persist(user, project, skill);
+    session.persist(project);
+    session.persist(skill);
+    session.persist(user);
     return project.getId();
   }
 }
