b091ea53e1cc5df33de982701602536e4da4fc05
Ben Manes <ben.manes@gmail.com>
2023-10-08 00:12:08 -0700

    upgrade to gradle 8.4

--- gradle/plugins/build.gradle.kts
+++ gradle/plugins/build.gradle.kts
@@ -10,7 +10,6 @@ java.toolchain.languageVersion = JavaLanguageVersion.of(11)
 dependencies {
   implementation(libs.bnd)
   implementation(libs.guava)
-  implementation(libs.coveralls)
   implementation(libs.sonarqube)
   implementation(libs.bundles.jmh)
   implementation(libs.bundles.pmd)
@@ -26,6 +25,9 @@ dependencies {
   implementation(platform(libs.kotlin.bom))
   implementation(platform(libs.jackson.bom))
   implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
+  implementation(libs.coveralls) {
+    exclude(group = "net.sourceforge.nekohtml", module = "nekohtml")
+  }
 
   libs.bundles.constraints.get().forEach { library ->
     constraints.add("implementation", library.module.toString())
