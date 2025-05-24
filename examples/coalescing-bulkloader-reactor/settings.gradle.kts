3693d7333268132febbf6fb496bca76f789dfa58
Ben Manes <ben.manes@gmail.com>
2025-05-24 12:05:30 -0700

    dependency updates

--- examples/coalescing-bulkloader-reactor/settings.gradle.kts
+++ examples/coalescing-bulkloader-reactor/settings.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   id("com.gradle.develocity") version "4.0.1"
   id("com.gradle.common-custom-user-data-gradle-plugin") version "2.2.1"
-  id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0-rc-1"
+  id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 dependencyResolutionManagement {
