f55fea436a73ed14d2d4604adbd8382a464957e9
Ben Manes <ben.manes@gmail.com>
2025-07-12 10:21:51 -0700

    minor touchups

---
+++ gradle/plugins/src/main/kotlin/quality/revapi.caffeine.gradle.kts
@@ -0,0 +1,28 @@
+import org.revapi.gradle.RevapiAcceptAllBreaksTask
+import org.revapi.gradle.RevapiVersionOverrideTask
+import org.revapi.gradle.RevapiAcceptBreakTask
+import org.revapi.gradle.RevapiAnalyzeTask
+import org.revapi.gradle.RevapiReportTask
+
+plugins {
+  `java-library`
+  id("org.revapi.revapi-gradle-plugin")
+}
+
+revapi {
+  setOldVersion(libs.caffeine.get().version)
+}
+
+val revapiTasks = listOf(
+  RevapiAcceptAllBreaksTask::class,
+  RevapiVersionOverrideTask::class,
+  RevapiAcceptBreakTask::class,
+  RevapiAnalyzeTask::class,
+  RevapiReportTask::class
+)
+
+revapiTasks.forEach { taskClass ->
+  tasks.withType(taskClass.java).configureEach {
+    incompatibleWithConfigurationCache()
+  }
+}
