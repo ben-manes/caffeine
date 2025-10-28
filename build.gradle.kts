6257608ee9a767d121e5fbecc6474254f3248067
Ben Manes <ben.manes@gmail.com>
2025-10-27 21:53:23 -0700

    Release 3.2.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,6 @@ allprojects {
   version(
     major = 3, // incompatible API changes
     minor = 2, // backwards-compatible additions
-    patch = 3, // backwards-compatible bug fixes
+    patch = 4, // backwards-compatible bug fixes
     releaseBuild = rootProject.hasProperty("release"))
 }
