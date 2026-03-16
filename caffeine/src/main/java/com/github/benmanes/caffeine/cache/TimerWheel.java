b57fbbf07678335cc9c1f50c99c990c8c4df6f74
Ben Manes <ben.manes@gmail.com>
2026-03-16 09:37:29 -0700

    Use bucket mask for start index in getExpirationDelay
    Consistent with expire() and peekAhead() which already use the
    timerWheel length mask rather than the span mask for tick-to-bucket
    conversion.

--- caffeine/src/main/java/com/github/benmanes/caffeine/cache/TimerWheel.java
+++ caffeine/src/main/java/com/github/benmanes/caffeine/cache/TimerWheel.java
@@ -249,9 +249,9 @@ final class TimerWheel<K, V> implements Iterable<Node<K, V>> {
       long ticks = (nanos >>> SHIFT[i]);
 
       long spanMask = SPANS[i] - 1;
-      int start = (int) (ticks & spanMask);
-      int end = start + timerWheel.length;
       int mask = timerWheel.length - 1;
+      int start = (int) (ticks & mask);
+      int end = start + timerWheel.length;
       for (int j = start; j < end; j++) {
         Node<K, V> sentinel = timerWheel[(j & mask)];
         Node<K, V> next = sentinel.getNextInVariableOrder();
